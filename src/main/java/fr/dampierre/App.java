6f92ec43b5a87f89e8a97ca054eb4d27b80dbe1f
massarre <massambasarr635@gmail.com>
2023-10-17 09:22:20 +0200

    commentaire test

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -1,8 +1,27 @@
 package fr.dampierre;
 
+import java.util.Scanner;
+
 public class App {
     
     public static void main(String[] args) {
+        //commentaire
+        Scanner sc= new Scanner (System.in); 
+        int salaireBase= 400;
+        int prime= 250;
+        int quota= 10;
+        System.out.println("entrer votre nombre de ventes par semaine:");
+        int nbreVentes= sc.nextInt();
+        if ( nbreVentes >= quota) {
+            
+            salaireBase= salaireBase + prime;
+            System.out.println("Félicitation!, votre salaire Finale est:"+ salaireBase);
+            
+        }else {
+            int venteManquantes= quota- nbreVentes; 
+           
+          System.out.println("votre salaire est:"+ salaireBase+ " Il vous manque "+ venteManquantes + " pour bénificier du prime");
+        }
     }
     
 }
